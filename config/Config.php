a1b7472148ca68097665fab068d530f7dbf897a7
root <root@ip-172-31-17-245.ap-northeast-1.compute.internal>
2016-06-11 23:57:15 -0400

    还是路径问题

--- config/Config.php
+++ config/Config.php
@@ -4,8 +4,7 @@
  * @author  gaochao <gaochao@baofeng.com>
  * @date    2016-06-03 18:08:32
  */
-
-define("BASE_PATH",$_SERVER["DOCUMENT_ROOT"].DIRECTORY_SEPARATOR);
+define("BASE_PATH",$_SERVER["DOCUMENT_ROOT"].$_SERVER["REQUEST_URI"]);
 
 define("SORT_PATH",BASE_PATH."sort".DIRECTORY_SEPARATOR);
 
