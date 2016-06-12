f00b9d901eca1df6a17a60a85296e83a1b35aa80
chaogao <chaogao2016@outlook.com>
2016-06-12 10:50:01 +0800

    require路径问题再修复

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once("./config/Config.php");
+require_once(dirname(__FILE__)."/config/Config.php");
 
-require_once("./common/Front.php");
+require_once(dirname(__FILE__)."/common/Front.php");
 
 Front::getInstance()->run();
 
